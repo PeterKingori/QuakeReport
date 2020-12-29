e6a300d041b9e36b33b2fe145f027757a26c34e0
Peter Kingori <kpndegwa@gmail.com>
2020-12-29 10:40:36 +0300

    1.9 Add URL property to Earthquake class and extract its value from the JSON response. Then add click listener on the list item in EarthquakeActivity.java to open website URL

--- app/src/main/java/com/peterkingori/quakereport/Earthquake.java
+++ app/src/main/java/com/peterkingori/quakereport/Earthquake.java
@@ -4,11 +4,13 @@ public class Earthquake {
     private double mMagnitude;
     private String mLocation;
     private long mTimeInMilliseconds;
+    private String mUrl;
 
-    public Earthquake(double magnitude, String location, long timeInMilliseconds) {
+    public Earthquake(double magnitude, String location, long timeInMilliseconds, String url) {
         mMagnitude = magnitude;
         mLocation = location;
         mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
     }
 
     public double getMagnitude() {
@@ -23,4 +25,7 @@ public class Earthquake {
         return mTimeInMilliseconds;
     }
 
+    public String getUrl() {
+        return mUrl;
+    }
 }
